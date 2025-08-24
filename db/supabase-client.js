d34a1239ab43a29d8bf54f3682865cd055d9ec1f
uksoldiers123-lab <uksoldiers123@gmail.com>
2025-08-24 17:44:16 -0400

    Update supabase-client.js

--- db/supabase-client.js
+++ db/supabase-client.js
@@ -1,8 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
-
-const SUPABASE_URL = process.env.SUPABASE_URL || 'https://xyz.supabase.co';
+const SUPABASE_URL = process.env.SUPABASE_URL || 'https://ziltrcaehpshkwganlcy.supabase.co';
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || 'public-anon-key';
-
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 module.exports = supabase;
