b13f63813f206302c3b3e52d740ca9b4cf92c289
uksoldiers123-lab <225610158+uksoldiers123-lab@users.noreply.github.com>
2025-08-25 16:35:54 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 const http = require('http');
-const app = require('./server'); // ensure this path matches your project
+const app = require('./server');
 
 const PORT = process.env.PORT || 3001;
-
 const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`API server listening on port ${PORT}`);
