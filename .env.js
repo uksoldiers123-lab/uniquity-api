42b44637bb9b2021aac6d2c64cf4463705c2cb64
uksoldiers123-lab <225610158+uksoldiers123-lab@users.noreply.github.com>
2025-09-01 14:37:35 -0400

    Update .env.js

--- .env.js
+++ .env.js
@@ -12,8 +12,7 @@ window.STRIPE_PUBLISHABLE_KEY = "pk_live_51Rv1X3By3HHUeuveqRsGvPb8Ob0bWjLg57VL08
 window.STRIPE_SECRET_KEY = "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";
 window.STRIPE_WEBHOOK_SIGNING_SECRET = "whsec_Jn1130PVT3LaVMySk6jLIGZKcF1cr5Bp";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpZmd1b3lxY2NvemxpanJ4Z2NmIiwicm9zZSI6ImFub24iLCJpYXQiOjE3NTY2MTg2MzUsImV4cCI6MjA3MjE5NDYzNX0.gwZnr8fKE7qXuLi8B5Merul3cVAXZ1r6SaWEUoAJWX0";
-// Ensure to use correct key
 window.SUPABASE_DB_URL = "postgresql://postgres:[#FREEMe24]@db.apbkobhfnmcqqzqeeqss.supabase.co:5432/postgres";
 window.SUPABASE_KEY = "your-supabase-key"; // Ensure to use correct key
-window.SUPABASE_SERVICE_ROLE_KEY = "sb_publishable_nlnBWEaRycn4fHAMBf26Vw_Gu8TwcZ3";
+window.SUPABASE_SERVICE_ROLE_KEY = "sb_secret_s4SxH76ocPQGIdrIFxyoFg_uVyZcLXD";
 window.SUPABASE_URL = "https://ziltrcaehpshkwganlcy.supabase.co";
