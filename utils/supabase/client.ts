a29e17cd9e1a64edabd65668708b1b286b930b1e
uksoldiers123-lab <225610158+uksoldiers123-lab@users.noreply.github.com>
2025-09-05 19:30:56 -0400

    Create client.ts

---
+++ utils/supabase/client.ts
@@ -0,0 +1,16 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+export const getUserOnClient = async () => {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return user;
+};
