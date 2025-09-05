10a7fac173540e847310b5c10f2a53fbe9b1e6d9
uksoldiers123-lab <225610158+uksoldiers123-lab@users.noreply.github.com>
2025-09-05 19:31:22 -0400

    Add files via upload

---
+++ utils/supabase/user.ts
@@ -0,0 +1,32 @@
+import { createClient } from "@/utils/supabase/server";
+import { User } from "@supabase/supabase-js";
+import { createHmac } from "crypto";
+import { cookies } from "next/headers";
+
+const hash = (input: string): string => {
+  const hmac = createHmac("sha256", Buffer.from(process.env.HMAC_KEY || "", 'hex'))
+  return hmac.update(input).digest("hex")
+}
+
+export const getUserOnServer = async (): Promise<[User | null, string | null]> => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return [null, null]
+  }
+
+  // When an auth.users is created, a trigger activates and creates a row in the
+  // public.users table. That trigger is also responsible for generating a secret
+  // using pgcrypto for the user.
+  const { data } = await supabase.from("users")
+    .select("secret")
+    .eq("id", user.id)
+  const secret: string | null = data ? hash(data[0].secret) : null
+
+  return [user, secret];
+};
